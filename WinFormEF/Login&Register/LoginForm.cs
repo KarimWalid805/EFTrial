467cfb8875db4468a7433610a5f8e296f7c0e1ca
KarimWalid805 <karimwalid1009@gmail.com>
2025-09-27 19:22:43 +0300

    added API functionality to login form

--- WinFormEF/Login&Register/LoginForm.cs
+++ WinFormEF/Login&Register/LoginForm.cs
@@ -8,6 +8,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -38,7 +40,8 @@ namespace WinFormEF
 
         private void LoginForm_Load(object sender, EventArgs e)
         {
-            con = new SqlConnection("Server=KARIMSPC;Database=OnlineDeliveryDB;Integrated Security=True;TrustServerCertificate=True;"); con.Open();
+            con = new SqlConnection("Server=KARIMSPC;Database=OnlineDeliveryDB;Integrated Security=True;TrustServerCertificate=True;");
+            con.Open();
         }
 
         private void registerbtn_Click(object sender, EventArgs e)
@@ -48,34 +51,25 @@ namespace WinFormEF
             registration.ShowDialog();
         }
 
-        private void loginbtn_Click(object sender, EventArgs e)
+        private async void loginbtn_Click(object sender, EventArgs e)
         {
             if (customerRadio.Checked)
             {
                 Session.UserType = "Customer";
                 if (passwordtxt.Text != string.Empty || usernametxt.Text != string.Empty)
                 {
-
-                    cmd = new SqlCommand("select * from customeraccount where username='" + usernametxt.Text + "' and password='" + passwordtxt.Text + "'", con);
-                    dr = cmd.ExecuteReader();
-                    if (dr.Read())
+                    bool loginSuccess = await LoginCustomerAsync(usernametxt.Text, passwordtxt.Text);
+                    if (loginSuccess)
                     {
-                        Session.UserName = dr["username"].ToString();
-                        Session.UserFirstName = dr["firstname"].ToString();
-                        Session.UserLastName = dr["lastname"].ToString();
-                        Session.Address = dr["address"].ToString();
-
-                        dr.Close();
                         this.Hide();
+
                         CustomersForm Customer = new CustomersForm();
                         Customer.ShowDialog();
                     }
                     else
                     {
-                        dr.Close();
-                        MessageBox.Show("No customer's Account avilable with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("No customer's Account available with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
-
                 }
                 else
                 {
@@ -89,23 +83,16 @@ namespace WinFormEF
                 if (passwordtxt.Text != string.Empty || usernametxt.Text != string.Empty)
                 {
 
-                    cmd = new SqlCommand("select * from driveraccount where username='" + usernametxt.Text + "' and password='" + passwordtxt.Text + "'", con);
-                    dr = cmd.ExecuteReader();
-                    if (dr.Read())
+                    bool loginSuccess = await LoginDriverAsync(usernametxt.Text, passwordtxt.Text);
+                    if (loginSuccess)
                     {
-
-                        Session.UserName = dr["username"].ToString();
-                        Session.UserFirstName = dr["firstname"].ToString();
-                        Session.UserLastName = dr["lastname"].ToString();
-                        Session.Address = dr["address"].ToString();
-                        dr.Close();
                         this.Hide();
+
                         DriversForm Driver = new DriversForm();
                         Driver.ShowDialog();
                     }
                     else
                     {
-                        dr.Close();
                         MessageBox.Show("No driver's Account avilable with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
 
@@ -123,19 +110,18 @@ namespace WinFormEF
                 if (passwordtxt.Text != string.Empty || usernametxt.Text != string.Empty)
                 {
 
-                    cmd = new SqlCommand("select * from adminaccount where username='" + usernametxt.Text + "' and password='" + passwordtxt.Text + "'", con);
-                    dr = cmd.ExecuteReader();
-                    if (dr.Read())
+                    bool loginSuccess = await LoginDriverAsync(usernametxt.Text, passwordtxt.Text);
+                    if (loginSuccess)
                     {
-                        dr.Close();
                         this.Hide();
-                        StartForm home = new StartForm();
-                        home.ShowDialog();
+
+                        DriversForm Driver = new DriversForm();
+                        Driver.ShowDialog();
                     }
                     else
                     {
                         dr.Close();
-                        MessageBox.Show("You are not an Admin!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("No driver's Account avilable with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
 
                 }
@@ -144,10 +130,120 @@ namespace WinFormEF
                     MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-
-
-
         }
+
+        public class LoginDto
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+
+        // Customer Login
+
+        public async Task<bool> LoginCustomerAsync(string username, string password)
+        {
+            using var client = new HttpClient();
+            var login = new LoginDto { Username = username, Password = password };
+
+            var response = await client.PostAsJsonAsync("https://localhost:7294/api/auth/customer", login);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<CustomerResult>();
+                // Set session values
+                Session.UserName = result.Username;
+                Session.UserFirstName = result.FirstName;
+                Session.UserLastName = result.LastName;
+                Session.Address = result.Address;
+                Session.UserType = "Customer";
+                return true;
+            }
+            else
+            {
+                // Handle error (e.g., show message)
+                return false;
+            }
+        }
+
+        public class CustomerResult
+        {
+            public string Username { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+        }
+        //----------------------------------------------------------------
+
+        // Driver Login
+        public async Task<bool> LoginDriverAsync(string username, string password)
+        {
+            using var client = new HttpClient();
+            var login = new LoginDto { Username = username, Password = password };
+
+            var response = await client.PostAsJsonAsync("https://localhost:7294/api/auth/driver", login);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<DriverResult>();
+                // Set session values
+                Session.UserName = result.Username;
+                Session.UserFirstName = result.FirstName;
+                Session.UserLastName = result.LastName;
+                Session.Address = result.Address;
+                Session.UserType = "Driver";
+                return true;
+            }
+            else
+            {
+                // Handle error (e.g., show message)
+                return false;
+            }
+        }
+
+        public class DriverResult
+        {
+            public string Username { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+        }
+
+        //---------------------------------------------------------------
+
+        // Admin Login
+
+        public async Task<bool> LoginAdminAsync(string username, string password)
+        {
+            using var client = new HttpClient();
+            var login = new LoginDto { Username = username, Password = password };
+            var response = await client.PostAsJsonAsync("https://localhost:7294/api/auth/admin", login);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<AdminResult>();
+                // Set session values
+                Session.UserName = result.Username;
+                Session.UserFirstName = result.FirstName;
+                Session.UserLastName = result.LastName;
+                Session.Address = result.Address;
+                Session.UserType = "Admin";
+                return true;
+            }
+            else
+            {
+                // Handle error (e.g., show message)
+                return false;
+            }
+        }
+
+        public class AdminResult
+        {
+            public string Username { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+        }
+
     }
 }
 
